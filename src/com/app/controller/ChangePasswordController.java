3949cd185669e9e8aa6f63fef50e00defe031d75
rafaelmanuel <rafaelmanuel00@gmail.com>
2016-12-19 20:49:01 +0800

    update

--- src/com/app/controller/ChangePasswordController.java
+++ src/com/app/controller/ChangePasswordController.java
@@ -49,16 +49,18 @@ public class ChangePasswordController implements Initializable, ControllerListen
 				if(!oldPasswordField.getText().trim().equals(newPasswordField.getText().trim())){
 					messageBox.setStyle("-fx-background-color:#2ecc71");
 					messageLabel.setText("You have successfully change your password");
+				}else{
+					messageBox.setStyle("-fx-background-color:#e74c3c;");
+					messageLabel.setText("You cannot reuse your old password.");
 				}
 			}
 		}else{
 			messageBox.setStyle("-fx-background-color:#e74c3c");
 			messageLabel.setText("Incorrect password!");
-			oldPasswordField.setStyle("-fx-border-color: #e74c3c;"
-					+ "-fx-border-width: 2;");
-		}
-		
+			oldPasswordField.setStyle("-fx-border-color: #e74c3c;-fx-border-width: 2;");
+		}	
 	}
+	
 	
 	@FXML
 	public void cancelOnAction(ActionEvent event){
@@ -67,64 +69,88 @@ public class ChangePasswordController implements Initializable, ControllerListen
 	
 	@FXML
 	public void handleOnKeyEvent(KeyEvent event){
-		oldPasswordField.setStyle("-fx-border-color: black;"
-				+ "-fx-border-width:0;");
-		if(!confirmPasswordField.getText().trim().equals(newPasswordField.getText().trim()))
-			buttonUpdate.setDisable(true);
-		else if((oldPasswordField.getText().trim().equals("") || newPasswordField.getText().trim().equals("")
+		
+		if((oldPasswordField.getText().trim().equals("") || newPasswordField.getText().trim().equals("")
 				|| confirmPasswordField.getText().trim().equals(""))) 
 			buttonUpdate.setDisable(true);
-		else
+		else if(!confirmPasswordField.getText().trim().equals(newPasswordField.getText().trim())){
+			buttonUpdate.setDisable(true);
+			confirmPasswordField.setStyle("-fx-border-color: #e74c3c;-fx-border-width: 2;");
+		}else
 			buttonUpdate.setDisable(false);
 		
-		if(!confirmPasswordField.getText().trim().equals("") ){
-			if(!confirmPasswordField.getText().trim().equals(newPasswordField.getText().trim()))
-				confirmPasswordField.setStyle("-fx-border-color: #e74c3c;"
-						+ "-fx-border-width: 2;");
-			else
-				confirmPasswordField.setStyle("-fx-border-color: #2ecc71;"
-						+ "-fx-border-width: 2;");
-		}
+		//new password
+		if(event.getSource().equals(newPasswordField)){
+			if(newPasswordField.getText().matches("[a-zA-Z0-9]{6,26}+")){
+				newPasswordField.setStyle("-fx-border-color: #2ecc71;-fx-border-width: 2;");
+				
+				messageBox.setStyle("-fx-background-color:#2ecc71;");
+				messageLabel.setText("New passwords are required to be a minimum of 6 characters in length.");
+			}else{
+				newPasswordField.setStyle("-fx-border-color: #e74c3c;-fx-border-width: 2;");
+				
+				messageBox.setStyle("-fx-background-color:#e67e22;");
+				messageLabel.setText("New passwords are required to be a minimum of 6 characters in length.");
+			}
 		
-	}
-	
-	@FXML
-	public void handeOnMouseExited(){
+		//confirm password
+		}else if(event.getSource().equals(confirmPasswordField)){
+			
+			if(confirmPasswordField.getText().trim().equals(newPasswordField.getText().trim())){
+				confirmPasswordField.setStyle("-fx-border-color: #2ecc71;-fx-border-width: 2;");
+				
+				messageBox.setStyle("-fx-background-color:#2ecc71;");
+				messageLabel.setText("The confirm password must match the new password entry.");
+				
+			}else{
+				confirmPasswordField.setStyle("-fx-border-color: #e74c3c;-fx-border-width: 2;");
+				
+				messageBox.setStyle("-fx-background-color:#e67e22;");
+				messageLabel.setText("The confirm password must match the new password entry.");
+
+			}
+		}
 		
 	}
 	
 	public void focusGained(FocusEvent event){
+		//if new password get focused
 		if(event.getClazz().equals("newPasswordField")){
-			newPasswordField.setStyle("-fx-border-color: black;"
-					+ "-fx-border-width:0;");
-			messageBox.setStyle("-fx-background-color:#16a085");
-			messageLabel.setText("New password have at least 6 characters.");
+			newPasswordField.setStyle("-fx-border-width:0;");
 			
-		}else{
-			confirmPasswordField.setStyle("-fx-border-color: black;"
-					+ "-fx-border-width:0;");
+			if(!newPasswordField.getText().matches("[a-zA-Z0-9]{6,26}+")){
+				messageBox.setStyle("-fx-background-color:#e67e22;");
+				messageLabel.setText("New passwords are required to be a minimum of 6 characters in length.");
+			}
+		//if confirm password get focused
+		}else if(event.getClazz().equals("confirmPasswordField")){
+			confirmPasswordField.setStyle("-fx-border-width:0;");
 			
-		}
+			//If confirm password not match the new password entry
+			if(!confirmPasswordField.getText().trim().equals(newPasswordField.getText().trim())){
+				messageBox.setStyle("-fx-background-color:#e67e22;");
+				messageLabel.setText("The confirm password must match the new password entry.");
+			}
+		//if old password get focused	
+		}else
+			oldPasswordField.setStyle("-fx-border-width:0;");
+			
 	}
 	
 	public void focusLost(FocusEvent event){
 		if(event.getClazz().equals("newPasswordField")){
-			if(!newPasswordField.getText().matches("[a-zA-Z0-9]{6,26}+")){
-				newPasswordField.setStyle("-fx-border-color: #e74c3c;"
-						+ "-fx-border-width: 2;");
-			}else{
-				newPasswordField.setStyle("-fx-border-color: #2ecc71;"
-						+ "-fx-border-width: 2;");
-			}
+			if(!newPasswordField.getText().matches("[a-zA-Z0-9]{6,26}+"))
+				newPasswordField.setStyle("-fx-border-color: #e74c3c;-fx-border-width: 2;");
+			else
+				newPasswordField.setStyle("-fx-border-color: #2ecc71;-fx-border-width: 2;");
+			
 		}else{
 			if(!confirmPasswordField.getText().trim().equals(newPasswordField.getText().trim())
-					|| confirmPasswordField.getText().trim().equals("")){
-				confirmPasswordField.setStyle("-fx-border-color: #e74c3c;"
-						+ "-fx-border-width: 2;");
-			}else{
-				confirmPasswordField.setStyle("-fx-border-color: #2ecc71;"
-						+ "-fx-border-width: 2;");
-			}
+					|| confirmPasswordField.getText().trim().equals(""))
+				confirmPasswordField.setStyle("-fx-border-color: #e74c3c;-fx-border-width: 2;");
+			else
+				confirmPasswordField.setStyle("-fx-border-color: #2ecc71;-fx-border-width: 2;");
+			
 		}
 	}
 
@@ -153,6 +179,17 @@ public class ChangePasswordController implements Initializable, ControllerListen
 					focusGained(event);
 				else
 					focusLost(event);	
+			}
+		});
+		
+		oldPasswordField.focusedProperty().addListener(new ChangeListener<Boolean>(){
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldProperty, Boolean newProperty) {
+				FocusEvent event = new FocusEvent();
+				event.setClazz("oldPasswordField");
+				if(newProperty)
+					focusGained(event);
+				
 			}
 		});
 		
